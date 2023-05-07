4e5ae1b18a6bbd5ff97a8cc44b2e8a6caae5aea7
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-07 11:58:03 +0600

    Server connected with mongoDb

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 7000;
+const uri = "mongodb+srv://saifulislam140301:YbrsidoWQ4dtmVbD@cluster0.8cnv71c.mongodb.net/?retryWrites=true&w=majority";
+
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+// saifulislam140301
+// YbrsidoWQ4dtmVbD
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const database = client.db("userDatabase");
+        const usersCollection = database.collection("users");
+
+        app.post('/users' , async(req,res)=>{
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send("SIMPLE CRUD IS RUNNING")
+})
+
+app.listen(port, () => {
+    console.log(`This Crud server running at port ${port}`);
+})
